c3bc64e5d52db21d0b1a8a792031edf65200fa50
Th-Calafatidis <th.kalafatidis@gmail.com>
2023-02-25 12:17:59 +0200

    Created InventoryManager script
    Created InventoryManager script responsible for displaying an inventory instance's items to the appropriate given UI.

---
+++ Assets/Main/Public/Theodore/Scripts/Inventory System/InventoryManager.cs
@@ -0,0 +1,40 @@
+// Date: 2/25/2023
+// Developer: Theodore Calafatidis
+//
+// Description: A script used to display an inventory instance on the appropriate UI.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    private Inventory _inventory;
+
+    private GameObject entryPrefab;
+
+
+    private void Awake()
+    {
+        entryPrefab = Resources.Load("itemSlotTemplate") as GameObject;
+    }
+
+    public void SetInventory(Inventory inventory, Transform containerUI)
+    {
+        _inventory = inventory;
+        RefreshInventoryItems(containerUI);
+    }
+
+    private void RefreshInventoryItems(Transform containerUI)
+    {
+        foreach (Item item in _inventory.GetItemList())
+        {
+            GameObject newEntry = Instantiate(entryPrefab, containerUI);
+
+            // Assign ItemData to the newEntry
+
+            newEntry.SetActive(true);
+        }
+    }
+}
--- Assets/Main/Public/Theodore/Scripts/Player/PlayerManager.cs
+++ Assets/Main/Public/Theodore/Scripts/Player/PlayerManager.cs
@@ -13,10 +13,34 @@ public class PlayerManager : MonoBehaviour
     private Inventory _inventory;
     public Inventory Inventory { get { return _inventory; } set { _inventory = value; } }
 
+    private Transform _inventoryContainer;
+
+    private InventoryManager _inventoryUI;
+
     private void Awake()
     {
         // Create a new inventory instance for the player when game starts
         _inventory = new Inventory();
 
+        LoadItems();
+
+        _inventoryContainer = GameObject.Find("InventorySlotContainer").transform;
+
+        _inventoryUI = GetComponent<InventoryManager>();
+
+        _inventoryUI.SetInventory(_inventory, _inventoryContainer);
+
+    }
+
+    // Debugging
+
+    public List<ItemData> ItemList;
+
+    public void LoadItems()
+    {
+        foreach(ItemData itemData in ItemList)
+        {
+            _inventory.AddItem(new Item(itemData));
+        }
     }
 }
